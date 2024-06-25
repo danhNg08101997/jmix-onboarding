698def88ed03a8d405b86ed44e77bc1379c6b913
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-06-26 01:43:52 +0700

    using enumerations

--- src/main/java/com/company/onboarding/view/department/DepartmentDetailView.java
+++ src/main/java/com/company/onboarding/view/department/DepartmentDetailView.java
@@ -3,14 +3,15 @@ package com.company.onboarding.view.department;
 import com.company.onboarding.entity.Department;
 import com.company.onboarding.view.main.MainView;
 import com.vaadin.flow.router.Route;
-import io.jmix.flowui.view.EditedEntityContainer;
-import io.jmix.flowui.view.StandardDetailView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.flowui.view.*;
 
 @Route(value = "departments/:id", layout = MainView.class)
 @ViewController("Department.detail")
 @ViewDescriptor("department-detail-view.xml")
 @EditedEntityContainer("departmentDc")
 public class DepartmentDetailView extends StandardDetailView<Department> {
+    @Subscribe
+    public void onInitEntity(final InitEntityEvent<Department> event) {
+        
+    }
 }
